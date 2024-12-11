0c6afc06465e95ce974947a4e6262f00af1ece05
gukin-han <gukin.dev@gmail.com>
2024-12-11 10:05:26 +0900

    회원 도메인 개발 end

---
+++ src/main/java/com/example/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package com.example.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+  // 데이터베이스가 아직 확정이 안되었다. 그래도 개발은 진행해야 하니 가장 단순한, 메모리 회원 저장소를 구현해서 우선 개발을 진행하자.
+  // 참고: `HashMap` 은 동시성 이슈가 발생할 수 있다. 이런 경우 `ConcurrentHashMap` 을 사용하자.
+  private static Map<Long, Member> store = new HashMap<>();
+
+  @Override
+  public void save(Member member) {
+    store.put(member.getId(), member);
+  }
+
+  @Override
+  public Member findById(Long id) {
+    return store.get(id);
+  }
+}
